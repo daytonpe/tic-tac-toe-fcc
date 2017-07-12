6421351c9e912ea429f65bb12ae4530296f9eeac
Pat Dayton <daytonpe14@gmail.com>
2017-07-12 15:00:00 -0500

    add scoreboard

--- fork.js
+++ fork.js
@@ -1,13 +1,13 @@
-var boxVals = ['O', 'X', 'X', 
-							'', '', '', 
-							'', 'O', ''];
+var boxVals = ['O', '', '', 
+							'', 'X', '', 
+							'', '', 'X'];
 
 var wins = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 var playerTeam = 'X';
 var computerTeam = 'O';
 
-function fork(){
+function fork(team){
 
 	//look at each of the nine boxes
 	for (var i in boxVals){
@@ -16,8 +16,8 @@ function fork(){
 		if (boxVals[i] != ''){
 			continue;
 		}
-		console.log('CHECKING: ' +i)
-		var winCount; //if I mark this box, how many winning paths can i set up? (either 0, 1, 2, or 3)
+		// console.log('CHECKING: ' +i)
+		var winCount=0; //if I mark this box, how many winning paths can i set up? (either 0, 1, 2, or 3)
 
 		//Plan from here on:
 		//loop through all win possibilities that contain the current box.
@@ -30,27 +30,43 @@ function fork(){
 			//if the win possibility contains the box we are checking, we see if it's a possible fork path
 			if (wins[j].includes(parseInt(i))){
 
-				//loop through the indexes in the win path
+
+				//build a symbol array for the win path i.e. ['X', '', 'O']
+				var winPathChars = [];
 				for (var k in wins[j]){
-
-					//make sure no parts of path have player team in it.
-					if (boxVals[k]==playerTeam){
-						continue;
-					}
-
-					//make sure it only has one of computer team (our) markings in row
-					if (wins[j].indexOf(computerTeam) == wins[j].lastIndexOf(computerTeam)){
-						console.log(wins[j]+ ' is a fork path via '+i);
-						console.log(wins[j].indexOf(computerTeam));
-						winCount++;
-					}
+					winPathChars.push(boxVals[wins[j][k]]);
 				}
+
+				//ensure winpath isn't empty
+				function isEmpty(str) { return str == '';}
+				if (winPathChars.every(isEmpty)){
+					continue;
+				}
+
+
+				//MAKE FORK: make sure no parts of path have player team in it.
+				//BLOCK FORK: make sure no parts of path have computer team in it.
+				if (winPathChars.includes(team)){
+					continue;
+				}
+				
+				//make sure there are two blank spaces in winpath
+				if (winPathChars.indexOf('') == winPathChars.lastIndexOf('')){ //if only one ''
+					continue;
+				}
+
+				//if it passes all the tests, then it is a valid fork option and we increment
+				winCount++;
+
+				//if wincount get's to 2, we play there
+				if (winCount==2){
+					return i;
+				}
+				// console.log(wins[j]+' '+winPathChars+' win count now at: '+winCount);
 			}
 		}
 	}
-
-
 	return -1;
 }
 
-console.log(fork())
+console.log(fork(computerTeam));
